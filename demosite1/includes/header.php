36fdcbe5e27309e34fb42af5b8890827459bf4f7
Ben Fuller <HawkBlade124@users.noreply.github.com>
2025-04-22 19:40:51 +0000

    made some more changes to website

--- demosite1/includes/header.php
+++ demosite1/includes/header.php
@@ -3,22 +3,44 @@
         <title>HTML Theme</title>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>        
         <link rel="stylesheet" href="/includes/css/master.css">
-        <link rel="stylesheet" href="/includes/css/fontawesome/css/all.css">
         <script src="/includes/js/master.js"></script>
+        <script src="https://kit.fontawesome.com/a4689790f6.js" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
     </head>
     <body>
-        <header>
-            <div class="logo">
-                <img src="images/logo.png" alt="Logo">
+        <div id="topHeader">
+            <div class="wrapperWidth d-flex justify-content-between align-items-center">                
+                <div class="headerLeft d-flex">
+                    <h1>Your Message here</h1>
+                </div>
+                <div id="rightSmallHead" class="d-flex justify-content-end align-items-center">
+                    <address class="d-flex align-items-center">
+                        <i class="fa-solid fa-location-dot"></i><a href="#"> 1234 Street Name, City, State, 12345</a>
+                    </address>
+                    <div class="socialMedia d-flex align-items-center">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                    </div>
+                </div>
             </div>
-            <nav class="navbar">
-                <ul id="navlist">
-                    <li><a href="#"><i class="fa-duotone fa-regular fa-house"></i> Home</a></li>
-                    <li><a href="#">Our Food</a></li>
-                    <li><a href="#">Catering</a></li>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+        </div>
+        <header class="d-flex align-items-center justify-content-between box-sizing">
+            <div class="wrapperWidth d-flex justify-content-between align-items-center w-100">
+                <div class="logo w-100">
+                    <img src="images/logo.png" alt="Logo">
+                </div>
+                <nav class="navbar w-100 d-flex justify-content-end">
+                    <ul id="navlist" class="d-flex align-items-center">
+                        <li><a href="#"><i class="fa-regular fa-house" aria-hidden="true"></i> Home</a></li>
+                        <li><a href="#">Our Food</a></li>
+                        <li><a href="#">Catering</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
--- demosite1/index.php
+++ demosite1/index.php
@@ -1,4 +1,5 @@
 <?php include_once 'includes/header.php'; ?>
+<main-content class="container">
     <div class="hero" class="even">
         <div class="blue heroContent">
             <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
@@ -6,41 +7,35 @@
             <button class="ctaButton">Order Now</button>
         </div>
         <div class="heroImage">
-            <img src="images/professional-service.jpg" alt="Hero Image">
+            <img class="w-100" src="images/professional-service.jpg" alt="Hero Image">
         </div>
     </div>
     <section id="menu" class="even">        
         <div id="menuWrapper" class="green wrapperWidth">
             <h2 class="wrapperHead">Food you love</h2>
-            <div class="innerWrapper">
-                <div class="menuItem">
+            <div class="innerWrapper d-flex justify-content-between flex-wrap">
+                <div class="menuItem box-sizing-content">
                     <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quis eius amet fugit repellendus laudantium officiis nihil impedit tenetur quos ipsum consectetur eos dicta voluptas cumque tempora, optio quisquam recusandae!</p>
+                    <img src="images/hamburger.jpg" alt="Man Eating a Hamburger" class="w-100 foodImage">
                 </div>
-                <div class="menuItem">                
-                    <img src="images/hamburger.jpg" alt="Man Eating a Hamburger">
-                </div>
-                <div class="menuItem">                
+                <div class="menuItem d-flex flex-column justify-content-between align-items-start">                
+                    <img src="images/lady-enjoying-food.jpg" alt="Lady Enjoying Food" class="w-100 foodImage">
                     <h3>Lorem Ipsum</h3>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure laudantium cum autem maiores unde voluptate recusandae, sunt quibusdam reprehenderit error impedit dicta optio ipsum porro. Quas repudiandae et sunt perspiciatis!</p>
                 </div>
-                <div class="menuItem">
-                    <img src="images/lady-enjoying-food.jpg" alt="Lady Enjoying Food">
-                </div>
-                <div class="menuItem">
+                <div class="menuItem box-sizing-content">
                     <h3>Lorem Ipsum</h3>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur possimus, saepe omnis quod velit numquam molestiae excepturi, nesciunt quae reprehenderit quis voluptatibus, earum maxime. Odit non quidem blanditiis nulla nobis.</p>
-                </div>
-                <div class="menuItem">
-                    <img src="images/group-enjoying-lunch.jpg" alt="Group Enjoying Lunch">
+                    <img src="images/group-enjoying-lunch.jpg" alt="Group Enjoying Lunch" class="w-100 foodImage">
                 </div>
             </div>
         </div>
     </section>
-    <section id="cards" class="odd">
+    <section id="cards" class="odd box-sizing-content">
         <h2 class="wrapperHead">Ways We Serve You</h2>
-        <div class="cardContainer wrapperWidth">
-            <div class="card">
+        <div class="cardContainer wrapperWidth d-flex flex-wrap justify-content-between">
+            <div class="card box-sizing-content">
                 <div class="cardWrapper">
                     <div class="cardImage">
                         <img src="images/service.jpg" alt="Man Walking with Plate">
@@ -52,7 +47,7 @@
                 </div>
                 <button class="cardButton">Learn More</button>
             </div>
-            <div class="card">
+            <div class="card box-sizing-content">
                 <div class="cardWrapper">
                     <div class="cardImage">
                         <img src="images/catering.jpg" alt="Table With Food for Catering">
@@ -64,7 +59,7 @@
                 </div>
                 <button class="cardButton">Learn More</button>
             </div>
-            <div class="card">
+            <div class="card box-sizing-content">
                 <div class="cardWrapper">
                     <div class="cardImage">
                         <img src="images/careers.jpg" alt="Smiling man with plate of food">
@@ -81,14 +76,14 @@
     <section id="menuCards">
         <div id="menuCardsWrapper">
             <a href="#">
-                <img src="images/"
+                <img src="images/">
             </a>
         </div>
     </section>
     <section id="testimonials" class="even">
         <h2 class="wrapperHead">What Our Customers Say</h2>
         <div class="testimonialContainer wrapperWidth">
-            <div class="testimonial">
+            <div class="testimonial active">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.</p>
                 <h3>John Doe</h3>
             </div>
@@ -111,4 +106,5 @@
             <button type="submit">Send Message</button>
         </form>
     </section>
+</main-content>
 <?php include_once 'includes/footer.php'; ?>
